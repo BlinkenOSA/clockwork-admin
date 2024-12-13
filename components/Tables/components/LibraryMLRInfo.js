7f71e7a2f518268793667f7a7409575327041bb5
JoshBone <jozsef.bone@gmail.com>
2024-12-13 11:22:05 +0100

    Update LibraryMLRInfo.js

--- components/Tables/components/LibraryMLRInfo.js
+++ components/Tables/components/LibraryMLRInfo.js
@@ -3,8 +3,6 @@ import {useData} from "../../../utils/hooks/useData";
 const LibraryMLRInfo = ({kohaID}) => {
 	const { data, loading } = useData(kohaID ? `/v1/research/requests/library/mlr/${kohaID}` : undefined);
 
-	console.log(data)
-
 	return (
 		data ? <div style={{fontSize: '12px'}}>{data}</div> : ''
 	)
