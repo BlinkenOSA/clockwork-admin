7f1a7a8599d45a133989ddcfc9026132828096e2
Josh <jozsef.bone@gmail.com>
2026-07-10 13:00:25 +0200

    Update FindingAidsEntityForm.js

--- components/Forms/fields/FindingAidsEntityForm.js
+++ components/Forms/fields/FindingAidsEntityForm.js
@@ -393,20 +393,20 @@ const Tab01Template = ({form, locale, readOnly, type}) => {
           form={form}
           mode={'toOriginal'}
           fieldName={'title'}
-          toField={'original_title'}
+          toField={'title_original'}
           disabled={readOnly}/>
       </Col>
       <Col xs={12}>
         <Form.Item
           label={renderLabelFlag(locale, 'Title - Original Language')}
-          name="original_title"
+          name="title_original"
           style={{marginBottom: 0}}>
           <Input disabled={readOnly}/>
         </Form.Item>
         <FormTranslateButton
           form={form}
           mode={'toEnglish'}
-          fieldName={'original_title'}
+          fieldName={'title_original'}
           toField={'title'}
           disabled={readOnly}/>
       </Col>
